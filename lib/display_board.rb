bd2b9096303a84aba0e950751be915f34309e834
Nathan Gezahegne <nathandaniel2222@gmail.com>
2018-06-26 21:22:51 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,10 +1,6 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
-
-
-def display_board
-   |   |   
------------
-   |   |   
------------
-   |   |
-end
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
